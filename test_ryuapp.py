fe220d8bc578857cb3d9be68f54d0cfed00e1e2a
ArielWu0203 <lucky020372@gmail.com>
2019-08-14 21:51:12 +0800

    reply count

--- test_ryuapp.py
+++ test_ryuapp.py
@@ -1,6 +1,6 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER,MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER,MAIN_DISPATCHER,DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.ofproto import ofproto_v1_3_parser
@@ -9,17 +9,71 @@ from ryu.lib.packet import ether_types
 from ryu.lib.packet import tcp
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import packet
-
+from operator import attrgetter
+from ryu.lib import hub
 
 class test_RyuApp(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-    lldp_struct = {}
     SA_struct = {}
 
     def __init__(self,*args,**kwargs):
         super(test_RyuApp,self).__init__(*args,**kwargs)
         self.mac_to_port = {}
         self.tcp_info = {}
+        self.datapaths = {}     
+        ## Monitor
+        self.monitor_thread = hub.spawn(self._monitor)
+    
+    @set_ev_cls(ofp_event.EventOFPStateChange,[MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def _state_change_handler(self, ev):
+        datapath = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if not datapath.id in self.datapaths:
+                self.logger.debug('register datapath: %016x', datapath.id)
+                self.datapaths[datapath.id] = datapath
+        elif ev.state == DEAD_DISPATCHER:
+            if datapath.id in self.datapaths:
+                self.logger.debug('unregister datapath: %016x', datapath.id)
+                del self.datapaths[datapath.id]
+    
+    # TODO : Monitor
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                self._request_stats(dp)
+            hub.sleep(3)
+
+    # TODO : Request flow stats
+    def _request_stats(self,datapath):
+        self.logger.debug('send flow stats request : %016x',datapath.id)
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        req = parser.OFPFlowStatsRequest(datapath = datapath,table_id = 2)
+        datapath.send_msg(req)
+        req = parser.OFPFlowStatsRequest(datapath = datapath,table_id = 3)
+        datapath.send_msg(req)
+
+    # TODO : Reply flow stats
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def _flow_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        print("datapath "
+              "table_id "
+              "ip               "
+              "packet_count ")
+        print("-------- "
+              "-------- "
+              "---------------- "
+              "------------ ")
+        if body[0].table_id==2:
+            for stat in sorted([flow for flow in body if flow.priority > 0],
+                               key=lambda flow: (flow.match['ipv4_dst'])):
+                print("%08d %08d %16s %08d " %(ev.msg.datapath.id,stat.table_id,stat.match['ipv4_dst'],stat.packet_count))
+        elif body[0].table_id==3:
+            for stat in sorted([flow for flow in body if flow.priority > 0],
+                               key=lambda flow: (flow.match['ipv4_src'])):
+                print("%08d %08d %16s %08d " %(ev.msg.datapath.id,stat.table_id,stat.match['ipv4_src'],stat.packet_count))
+     
 
     # TODO : Add rules.
     def add_flow(self,datapath,table_id,cookie,cookie_mask,priority,match,actions):
