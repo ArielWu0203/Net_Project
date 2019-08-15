0f522c98f6f4a1075a5a665dc43504d9510b1095
ArielWu0203 <lucky020372@gmail.com>
2019-08-15 18:29:31 +0800

    table 2&3 clean

--- test_ryuapp.py
+++ test_ryuapp.py
@@ -19,11 +19,20 @@ class test_RyuApp(app_manager.RyuApp):
     def __init__(self,*args,**kwargs):
         super(test_RyuApp,self).__init__(*args,**kwargs)
         self.mac_to_port = {}
-        self.tcp_info = {}
+        #self.tcp_info = {}
+        
         self.datapaths = {}     
+
+        self.sa_table = {}
+        self.ack_table = {}
         ## Monitor
+        self.time = 10
         self.monitor_thread = hub.spawn(self._monitor)
-    
+ 
+        ## Clean
+        self.clean_time = 60
+        self.clean_thread = hub.spawn(self._clean)
+
     @set_ev_cls(ofp_event.EventOFPStateChange,[MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
         datapath = ev.datapath
@@ -36,12 +45,26 @@ class test_RyuApp(app_manager.RyuApp):
                 self.logger.debug('unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
     
+    # TODO : Clean
+    def _clean(self):
+        while True:
+            for dp in self.datapaths.values():
+                if dp.id == 1:
+                    self.sa_table.clear()
+                    self.ack_table.clear()
+                    self.del_flow(dp,1,1,0xFFFFFFFFFFFFFFFF)
+                    self.del_flow(dp,2,1,0xFFFFFFFFFFFFFFFF)
+                    self.del_flow(dp,3,1,0xFFFFFFFFFFFFFFFF)
+
+            hub.sleep(self.clean_time)
+
     # TODO : Monitor
     def _monitor(self):
         while True:
             for dp in self.datapaths.values():
-                self._request_stats(dp)
-            hub.sleep(3)
+                if dp.id == 1:
+                    self._request_stats(dp)
+            hub.sleep(self.time)
 
     # TODO : Request flow stats
     def _request_stats(self,datapath):
@@ -57,23 +80,57 @@ class test_RyuApp(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
         body = ev.msg.body
-        print("datapath "
-              "table_id "
-              "ip               "
-              "packet_count ")
-        print("-------- "
-              "-------- "
-              "---------------- "
-              "------------ ")
-        if body[0].table_id==2:
-            for stat in sorted([flow for flow in body if flow.priority > 0],
-                               key=lambda flow: (flow.match['ipv4_dst'])):
-                print("%08d %08d %16s %08d " %(ev.msg.datapath.id,stat.table_id,stat.match['ipv4_dst'],stat.packet_count))
-        elif body[0].table_id==3:
-            for stat in sorted([flow for flow in body if flow.priority > 0],
-                               key=lambda flow: (flow.match['ipv4_src'])):
-                print("%08d %08d %16s %08d " %(ev.msg.datapath.id,stat.table_id,stat.match['ipv4_src'],stat.packet_count))
+        
+        if ev.msg.datapath.id == 1:
+
+            self.logger.info("datapath "
+                  "table_id "
+                  "ip               "
+                  "packet_count ")
+            self.logger.info("-------- "
+                  "-------- "
+                  "---------------- "
+                  "------------ ")
+
+            if body[0].table_id==2 and ev.msg.datapath.id == 1:
+                for stat in sorted([flow for flow in body if flow.priority > 0],
+                                   key=lambda flow: (flow.match['ipv4_dst'])):
+                    self._collect(stat.table_id,stat.match['ipv4_dst'],stat.packet_count)
+                    self.logger.info("%08d %08d %16s %08d " %(ev.msg.datapath.id,stat.table_id,stat.match['ipv4_dst'],stat.packet_count))
+            elif body[0].table_id==3 and ev.msg.datapath.id == 1:
+                for stat in sorted([flow for flow in body if flow.priority > 0],
+                                   key=lambda flow: (flow.match['ipv4_src'])):
+                    self._collect(stat.table_id,stat.match['ipv4_src'],stat.packet_count)
+                    self.logger.info("%08d %08d %16s %08d " %(ev.msg.datapath.id,stat.table_id,stat.match['ipv4_src'],stat.packet_count))
      
+            self._detect(ev.msg.datapath)
+
+    # TODO : Calculate Syn/ack & ACK
+    def _collect(self,table_id,ipv4,packet_count):
+        if table_id == 2:
+            self.sa_table.setdefault(ipv4,None)
+            self.sa_table[ipv4]=packet_count
+            #self.logger.info('sa_table',self.sa_table)
+        elif table_id == 3:
+            self.ack_table.setdefault(ipv4,None)
+            self.ack_table[ipv4]=packet_count
+            #self.logger.info('ack_table',self.ack_table)
+
+
+    # TODO : Detect & drop packets
+    def _detect(self,datapath):
+        
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        
+        keys = self.ack_table.keys()
+        for ip in keys:
+            if self.ack_table[ip] == 0 and self.sa_table[ip] > 0:
+                print(datapath.id)
+                match = parser.OFPMatch(eth_type = 0x0800,ip_proto=6,tcp_flags=0x02,ipv4_src=ip)
+                inst = []
+                mod = parser.OFPFlowMod(datapath = datapath,table_id=1,cookie=1,cookie_mask=0,priority = 1,match = match,instructions = inst)
+                datapath.send_msg(mod)
 
     # TODO : Add rules.
     def add_flow(self,datapath,table_id,cookie,cookie_mask,priority,match,actions):
@@ -84,11 +141,12 @@ class test_RyuApp(app_manager.RyuApp):
         datapath.send_msg(mod)
 
     # TODO : Delete rules.
-    def del_flow(self,datapath,cookie,cookie_mask):
+    def del_flow(self,datapath,table_id,cookie,cookie_mask):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         
-        mod = parser.OFPFlowMod(datapath = datapath,command=ofproto.OFPFC_DELETE,cookie=cookie,cookie_mask=cookie_mask,table_id=ofproto.OFPTT_ALL,out_port=ofproto.OFPP_ANY,out_group=ofproto.OFPG_ANY)
+        mod = parser.OFPFlowMod(datapath = datapath,command=ofproto.OFPFC_DELETE,cookie=cookie,cookie_mask=cookie_mask,table_id=table_id,out_port=ofproto.OFPP_ANY,out_group=ofproto.OFPG_ANY)
+        #table_id=ofproto.OFPTT_ALL
         datapath.send_msg(mod)
     
     # TODO : Modify rules.
@@ -189,14 +247,14 @@ class test_RyuApp(app_manager.RyuApp):
                     
                     match = parser.OFPMatch(eth_type = 0x0800,ip_proto=6,tcp_flags=0x12,ipv4_dst=pkt_ipv4.dst)
                     inst = [parser.OFPInstructionGotoTable(table_id=4)]
-                    mod = parser.OFPFlowMod(datapath = datapath,table_id=2,priority = 1,match = match,instructions = inst)
+                    mod = parser.OFPFlowMod(datapath = datapath,table_id=2,cookie=1,cookie_mask=0,priority = 1,match = match,instructions = inst)
                     datapath.send_msg(mod)
 
                     #print(self.SA_struct)
                     
                     match = parser.OFPMatch(eth_type = 0x0800,ip_proto=6,tcp_flags=0x10,ipv4_src=pkt_ipv4.dst)
                     inst = [parser.OFPInstructionGotoTable(table_id=4)]
-                    mod = parser.OFPFlowMod(datapath = datapath,table_id=3,priority = 1,match = match,instructions = inst)
+                    mod = parser.OFPFlowMod(datapath = datapath,table_id=3,cookie=1,cookie_mask=0,priority = 1,match = match,instructions = inst)
                     datapath.send_msg(mod)
 
                     #print(datapath.id,pkt_ipv4.src,pkt_ipv4.dst,pkt_tcp.bits)
